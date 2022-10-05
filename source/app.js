44a39ee2aae8a02843ddad72c5b410d93fbfe6c9
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-05 16:15:52 +0300

    delete everything, cause why not

--- source/app.js
+++
@@ -1,9 +0,0 @@
-function oopsAlert() {
-  alert("This one is not ready yet:)");
-}
-
-const oops = document.querySelector("#oops");
-const o = document.querySelector("#o");
-
-o.addEventListener("click", oopsAlert);
-oops.addEventListener("click", oopsAlert);
